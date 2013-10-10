d9302e317ceaec6fffaf97496734af3215a29466
magnus.andersson@mollyware.se <magnus.andersson@mollyware.se@ce06153b-8440-0b4c-231a-5aa74c0d1022>
2013-10-10 12:21:19 +0000

    - Removed Schemas from project. Instead deployed packaged versions to VGR Internal Nexus.

--- trunk/core-bc/composites/svc/src/main/java/se/vgregion/service/pdl/PdlServiceImpl.java
+++ trunk/core-bc/composites/svc/src/main/java/se/vgregion/service/pdl/PdlServiceImpl.java
@@ -1,13 +1,26 @@
 package se.vgregion.service.pdl;
 
 import org.springframework.stereotype.Service;
+import se.riv.ehr.blocking.querying.getblocksforpatient.v2.rivtabp21.GetBlocksForPatientResponderInterface;
+import se.riv.ehr.blocking.querying.getblocksforpatient.v2.rivtabp21.GetBlocksForPatientResponderService;
 import se.vgregion.pdl.domain.PdlContext;
 import se.vgregion.pdl.domain.PdlReport;
 
+import javax.annotation.Resource;
+
 @Service
-public class PdlServiceImpl {
+public class PdlServiceImpl implements PdlService {
+
+    @Resource(name="blocksForPatient")
+    private GetBlocksForPatientResponderService blocksForPatient;
+
+    public PdlServiceImpl() {
+    }
 
     public PdlReport pdlReport(PdlContext ctx) {
+        GetBlocksForPatientResponderInterface port = blocksForPatient.getGetBlocksForPatientResponderPort();
+
+
         return new PdlReport();
     }
 }
