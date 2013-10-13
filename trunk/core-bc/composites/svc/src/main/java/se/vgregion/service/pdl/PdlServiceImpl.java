1fa3341328143cb549403860345706014b18d62d
magnus.andersson@mollyware.se <magnus.andersson@mollyware.se@ce06153b-8440-0b4c-231a-5aa74c0d1022>
2013-10-13 23:28:03 +0000

    - Added async behavior and refactored into Report utility class - Implemented fallback behavior if underlying PDL service is not responding

--- trunk/core-bc/composites/svc/src/main/java/se/vgregion/service/pdl/PdlServiceImpl.java
+++ trunk/core-bc/composites/svc/src/main/java/se/vgregion/service/pdl/PdlServiceImpl.java
@@ -1,22 +1,15 @@
 package se.vgregion.service.pdl;
 
 import org.springframework.stereotype.Service;
-import se.riv.ehr.blocking.accesscontrol.checkblocks.v3.rivtabp21.CheckBlocksResponderInterface;
 import se.riv.ehr.blocking.accesscontrol.checkblocks.v3.rivtabp21.CheckBlocksResponderService;
-import se.riv.ehr.blocking.accesscontrol.checkblocksresponder.v3.CheckBlocksResponseType;
-import se.riv.ehr.patientconsent.accesscontrol.checkconsent.v1.rivtabp21.CheckConsentResponderInterface;
 import se.riv.ehr.patientconsent.accesscontrol.checkconsent.v1.rivtabp21.CheckConsentResponderService;
-import se.riv.ehr.patientconsent.accesscontrol.checkconsentresponder.v1.CheckConsentResponseType;
-import se.riv.ehr.patientrelationship.accesscontrol.checkpatientrelation.v1.rivtabp21.CheckPatientRelationResponderInterface;
 import se.riv.ehr.patientrelationship.accesscontrol.checkpatientrelation.v1.rivtabp21.CheckPatientRelationResponderService;
-import se.riv.ehr.patientrelationship.accesscontrol.checkpatientrelationresponder.v1.CheckPatientRelationResponseType;
-import se.vgregion.domain.pdl.*;
+import se.vgregion.domain.pdl.PatientEngagement;
+import se.vgregion.domain.pdl.PdlAssertion;
+import se.vgregion.domain.pdl.PdlContext;
+import se.vgregion.domain.pdl.PdlReport;
 
 import javax.annotation.Resource;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 @Service
@@ -29,54 +22,29 @@ public class PdlServiceImpl implements PdlService {
     @Resource(name = "relationshipWithPatient")
     private CheckPatientRelationResponderService relationshipWithPatient;
 
-    public PdlServiceImpl() {
+    @Override
+    public PdlReport pdlReport(final PdlContext ctx) {
+        return Report.asyncReport(ctx, blocksForPatient, consentForPatient, relationshipWithPatient);
     }
 
-    public static XMLGregorianCalendar xmlDate() {
-        try {
-            GregorianCalendar c = new GregorianCalendar();
-            return DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
-        } catch (DatatypeConfigurationException e) {
-            throw new RuntimeException("Unable to create XMLDate", e);
-        }
+    @Override
+    public PdlReport patientConsent(PdlContext ctx) {
+        throw new IllegalStateException("Not implemented");
     }
 
-    public PdlReport pdlReport(PdlContext ctx) {
-
-        List<CheckedBlock> checkedBlocks = checkBlocks(ctx);
-        CheckedConsent checkedConsent = checkConsent(ctx);
-        boolean hasRelationship = checkRelationship(ctx);
-
-        return new PdlReport(checkedBlocks, checkedConsent, hasRelationship);
+    @Override
+    public PdlReport patientRelationship(PdlContext ctx) {
+        throw new IllegalStateException("Not implemented");
     }
 
-    private boolean checkRelationship(PdlContext ctx) {
-        CheckPatientRelationResponderInterface relationshipPort =
-                relationshipWithPatient.getCheckPatientRelationResponderPort();
-        CheckPatientRelationResponseType relationshipResponse =
-                relationshipPort.checkPatientRelation(ctx.careProviderHsaId, Relationship.checkRelationshipRequest(ctx));
-
-        return relationshipResponse.getCheckResultType().isHasPatientrelation();
+    @Override
+    public PdlReport unblockInformation(PdlContext ctx, String blockId, UnblockType unblockType, String unblockComment) {
+        throw new IllegalStateException("Not implemented");
     }
 
-    private List<CheckedBlock> checkBlocks(PdlContext ctx) {
-        CheckBlocksResponderInterface blockPort = blocksForPatient.getCheckBlocksResponderPort();
-        CheckBlocksResponseType blockResponse =
-                blockPort.checkBlocks(ctx.careProviderHsaId, Blocking.checkBlocksRequest(ctx));
-
-        return Blocking.asCheckedBlocks(ctx, blockResponse);
+    @Override
+    public PdlAssertion affirmChoises(PdlContext ctx, PdlReport report, List<PatientEngagement> engagements) {
+        throw new IllegalStateException("Not implemented");
     }
-
-    public CheckedConsent checkConsent(PdlContext ctx) {
-        CheckConsentResponderInterface consentPort =
-                consentForPatient.getCheckConsentResponderPort();
-        CheckConsentResponseType consentResponse =
-                consentPort.checkConsent(ctx.careProviderHsaId, Consent.checkConsentRequest(ctx));
-
-        return Consent.asCheckedConsent(consentResponse);
-    }
-
-
-
 
 }
