1033df321597ce9d18e9bdfccca2a125665098e8
magnus.andersson@mollyware.se <magnus.andersson@mollyware.se@ce06153b-8440-0b4c-231a-5aa74c0d1022>
2013-11-12 13:20:31 +0000

    - Troubleshooting VGR Jenkins build failure

--- trunk/core-bc/composites/types/src/main/java/se/vgregion/domain/pdl/CheckedConsent.java
+++ trunk/core-bc/composites/types/src/main/java/se/vgregion/domain/pdl/CheckedConsent.java
@@ -6,9 +6,9 @@ public class CheckedConsent implements Serializable {
     private static final long serialVersionUID = 7700912160877500493L;
 
     public final PdlReport.ConsentType consentType;
-    public final boolean hasConsent;
+    public final Boolean hasConsent;
 
-    public CheckedConsent(PdlReport.ConsentType consentType, boolean hasConsent) {
+    public CheckedConsent(PdlReport.ConsentType consentType, Boolean hasConsent) {
         this.consentType = consentType;
         this.hasConsent = hasConsent;
     }
