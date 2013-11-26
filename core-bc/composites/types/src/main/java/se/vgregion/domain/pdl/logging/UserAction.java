9d73595ca3724dbb1c7472aff4b85321f48dbce7
claes.lundahl <claes.lundahl@gmail.com>
2013-11-26 08:49:29 +0000

    Missing enum type breaks build. Checking it in.

---
+++ core-bc/composites/types/src/main/java/se/vgregion/domain/pdl/logging/UserAction.java
@@ -0,0 +1,13 @@
+package se.vgregion.domain.pdl.logging;
+
+public enum UserAction {
+    SEARCH,
+    RELATION,
+    CONSENT,
+    CONSENT_EMERGENCY,
+    BLOCK,
+    BLOCK_EMERGENCY,
+    OTHER_CARE_UNITS,
+    OTHER_CARE_PROVIDERS,
+    INFORMATION_CHOICE
+}
