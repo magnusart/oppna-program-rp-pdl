cf004401b10e3dd16ef8c99449ced092dbaac714
magnus.andersson@mollyware.se <magnus.andersson@mollyware.se@ce06153b-8440-0b4c-231a-5aa74c0d1022>
2013-12-02 16:43:48 +0000

    - Adding WithSelection to Information types.

--- trunk/core-bc/composites/types/src/main/java/se/vgregion/domain/pdl/decorators/WithSelection.java
+++ trunk/core-bc/composites/types/src/main/java/se/vgregion/domain/pdl/decorators/WithSelection.java
@@ -2,7 +2,9 @@ package se.vgregion.domain.pdl.decorators;
 
 import java.io.Serializable;
 
-public class WithSelection<T extends Serializable> implements Serializable {
+public class WithSelection<T extends Serializable> implements Serializable, Comparable<WithSelection<T>> {
+    private static final long serialVersionUID = -6925415052903676453L;
+
     public final String id;
     public final boolean selected;
     public final T value;
@@ -25,6 +27,19 @@ public class WithSelection<T extends Serializable> implements Serializable {
         return new WithSelection<T>(id, false, newValue);
     }
 
+    public static <N extends Serializable, N1 extends N> WithSelection<N> getDeselected(N1 value) {
+        return new WithSelection<N>(false, value);
+    }
+
+    @Override
+    public String toString() {
+        return "WithSelection{" +
+                "id='" + id + '\'' +
+                ", selected=" + selected +
+                ", value=" + value +
+                '}';
+    }
+
     public WithSelection<T> mapSelection(boolean newSelection) {
         return new WithSelection<T>(id, newSelection, value);
     }
@@ -33,4 +48,30 @@ public class WithSelection<T extends Serializable> implements Serializable {
         return new WithSelection<T>(id, selected, newValue);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof WithSelection)) return false;
+
+        WithSelection that = (WithSelection) o;
+
+        if (selected != that.selected) return false;
+        if (!id.equals(that.id)) return false;
+        if (!value.equals(that.value)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (selected ? 1 : 0);
+        result = 31 * result + value.hashCode();
+        return result;
+    }
+
+    @Override
+    public int compareTo(WithSelection<T> o) {
+        return id.compareTo(o.id);
+    }
 }
