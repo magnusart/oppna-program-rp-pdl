7480023a669cd83aa0f22d5c3e2150a6e8656d8a
patrik.bergstrom@knowit.se <patrik.bergstrom@knowit.se@ce06153b-8440-0b4c-231a-5aa74c0d1022>
2014-03-31 09:17:28 +0000

    @Ignore integration tests as they require properties for e.g. credentials stored in user home directory.

--- trunk/pdl/composites/svc/src/test/java/se/vgregion/service/kiv/KivLdapServiceIT.java
+++ trunk/pdl/composites/svc/src/test/java/se/vgregion/service/kiv/KivLdapServiceIT.java
@@ -1,5 +1,6 @@
 package se.vgregion.service.kiv;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,10 @@ import se.vgregion.domain.decorators.WithOutcome;
 
 import static org.junit.Assert.*;
 
+/**
+ * Run these tests when needed. They are set as @Ignore as they need a properties file in your user home with
+ * e.g. credentials.
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:ldap-context.xml", "classpath:ldap-context-properties.xml"})
 public class KivLdapServiceIT {
@@ -18,6 +23,7 @@ public class KivLdapServiceIT {
     private KivLdapService kivLdapService;
 
     @Test
+    @Ignore
     public void testFetchHsaId() throws Exception {
 
         WithOutcome<String> hsaId = kivLdapService.getHsaIdByVgrId("patbe5");
@@ -27,12 +33,14 @@ public class KivLdapServiceIT {
     }
 
     @Test
+    @Ignore
     public void testFetchHsaIdExceptionNotFound() {
         WithOutcome<String> outcome = kivLdapService.getHsaIdByVgrId("asdfasdf");// Not found
         assertTrue(outcome.outcome.equals(Outcome.UNFULFILLED_FAILURE));
     }
 
     @Test
+    @Ignore
     public void testFetchHsaIdExceptionMultipleMatches() {
         try {
             WithOutcome<String> outcome = kivLdapService.getHsaIdByVgrId("mar*");// Multiple matches
